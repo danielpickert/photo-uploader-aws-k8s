1a2b0843b09e386f9a5ae2cd8eb0c4b5b127647e
Daniel Pickert <danielpickert@MacBookPro.attlocal.net>
2025-09-17 23:58:36 -0400

    made changes to eks file and updated ci-cd pipeline

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,37 +1,60 @@
-# This uses the official AWS EKS module. You can change configuration as needed.
+# Get default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get default subnets
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+  filter {
+    name   = "availability-zone"
+    values = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1f"]
+  }
+}
+
+# Terraform AWS EKS module v20+ compatible
 module "eks" {
-source = "terraform-aws-modules/eks/aws"
-version = "~> 20.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.1"
 
+  cluster_name    = "${var.project}-eks"
+  cluster_version = "1.30"
 
-cluster_name = "${var.project}-eks"
-cluster_version = "1.27"
-subnets = [] # If empty, module creates default subnets in the default VPC. For production, pass explicit VPC/subnets.
+  vpc_id     = data.aws_vpc.default.id
+  subnet_ids = data.aws_subnets.default.ids
 
+  # EKS Managed Node Group(s)
+  eks_managed_node_groups = {
+    demo_nodes = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
 
-node_groups = {
-on_demand = {
-desired_capacity = 2
-max_capacity = 3
-min_capacity = 1
+      instance_types = ["t3.medium"]
+    }
+  }
 
-
-instance_types = ["t3.medium"]
+  tags = {
+    Terraform   = "true"
+    Environment = "demo"
+  }
 }
-}
-}
 
-
+# Outputs for connecting to cluster
 output "cluster_endpoint" {
-value = module.eks.cluster_endpoint
+  description = "Endpoint for EKS control plane"
+  value       = module.eks.cluster_endpoint
 }
-
 
 output "cluster_certificate_authority_data" {
-value = module.eks.cluster_certificate_authority_data
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = module.eks.cluster_certificate_authority_data
 }
 
-
-output "kubeconfig" {
-value = module.eks.kubeconfig
+output "cluster_name" {
+  description = "The name/id of the EKS cluster"
+  value       = module.eks.cluster_name
 }
